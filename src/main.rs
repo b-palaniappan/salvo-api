6e21c087b07e5557d8f2d3d7113c23c41fbc5eea
Balasubramanian Palaniappan <b.palaniappan@hotmail.com>
2023-03-11 22:11:59 -0600

    updates added

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
+use chrono::{SecondsFormat, Utc};
 use salvo::prelude::*;
 use serde::{Deserialize, Serialize};
-use chrono::{Utc, SecondsFormat};
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Message {
     status: String,
     message: String,
-    timestamp: String
+    timestamp: String,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -14,7 +14,7 @@ struct ApiError {
     status: i32,
     timestamp: String,
     message: String,
-    debug_message: String
+    debug_message: String,
 }
 
 type AppResponse<T> = Result<T, ApiError>;
@@ -26,7 +26,7 @@ impl Writer for ApiError {
             status: 500,
             timestamp: Utc::now().to_rfc3339_opts(SecondsFormat::Micros, true),
             message: "Internal server error".to_owned(),
-            debug_message: "Internal server error try after some time".to_owned()
+            debug_message: "Internal server error try after some time".to_string(),
         };
         res.render(Json(error));
     }
@@ -35,10 +35,10 @@ impl Writer for ApiError {
 #[handler]
 async fn hello_world(res: &mut Response) -> AppResponse<()> {
     let message = Message {
-            status: "ok".to_string(),
-            message: "working".to_string(),
-            timestamp: Utc::now().to_rfc3339_opts(SecondsFormat::Micros, true)
-        };
+        status: "ok".to_string(),
+        message: "working".to_string(),
+        timestamp: Utc::now().to_rfc3339_opts(SecondsFormat::Micros, true),
+    };
     res.render(Json(message));
     Ok(())
 }
@@ -46,5 +46,7 @@ async fn hello_world(res: &mut Response) -> AppResponse<()> {
 #[tokio::main]
 async fn main() {
     let router = Router::new().get(hello_world);
-    Server::new(TcpListener::bind("127.0.0.1:8080")).serve(router).await;   
+    Server::new(TcpListener::bind("127.0.0.1:8080"))
+        .serve(router)
+        .await;
 }
